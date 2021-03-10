088d3cc0d8c7b62f04a8ede2f16822f10d68ae14
Faarid <faarid.me@gmail.com>
2021-03-10 05:11:13 -0800

    Spinner

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,11 +3,13 @@ import {MatButtonModule} from '@angular/material/button'
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
 import {MatIconModule} from '@angular/material/icon';
 import {MatBadgeModule} from '@angular/material/badge';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 const MaterialComponents = [
   MatButtonModule,
   MatButtonToggleModule,
   MatBadgeModule,
+  MatProgressSpinnerModule,
   MatIconModule
 ];
 
