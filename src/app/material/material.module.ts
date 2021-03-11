2fcdbe075e2e1fcc11ee19ca2ce897cfcd822909
Faarid <faarid.me@gmail.com>
2021-03-10 22:21:57 -0800

    Input

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -13,10 +13,16 @@ import {MatGridListModule} from '@angular/material/grid-list';
 import {MatExpansionModule} from '@angular/material/expansion';
 import {MatCardModule} from '@angular/material/card';
 import {MatTabsModule} from '@angular/material/tabs';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 const MaterialComponents = [
   MatButtonModule,
+  MatInputModule,
+  MatFormFieldModule,
   MatCardModule,
+  MatStepperModule,
   MatTabsModule,
   MatButtonToggleModule,
   MatBadgeModule,
